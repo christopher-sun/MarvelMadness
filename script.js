9fcdea58cfa9e6bdb54a7e4682da070b2f6b4d05
Sammi Hudock <Sammi Hudock>
2019-01-18 13:41:58 -0500

    closer api

--- script.js
+++ script.js
@@ -1,27 +1,42 @@
 const app = document.getElementById('root');
 
+
+
 const container = document.createElement('div');
 container.setAttribute('class', 'container');
 
+
 app.appendChild(container);
 
+function makeid() {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (var i = 0; i < 15; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+}
+
+
 var request = new XMLHttpRequest();
-request.open('GET', 'https://ghibliapi.herokuapp.com/films', true);
+request.open('GET', 'https://gateway.marvel.com:443/v1/public/events?limit=5&ts=123456789&apikey=e147157469eaafd8c42877fad76e062b&hash=9c0442a67cad74b32745fb23ba5fbb73', true);
+// https://gateway.marvel.com:443/v1/public/events?apikey=e147157469eaafd8c42877fad76e062b
 request.onload = function () {
 
   // Begin accessing JSON data here
   var data = JSON.parse(this.response);
   if (request.status >= 200 && request.status < 400) {
-    data.forEach(movie => {
+    data.forEach(comic => {
       const card = document.createElement('div');
       card.setAttribute('class', 'card');
 
       const h1 = document.createElement('h1');
-      h1.textContent = movie.title;
+      h1.textContent = comic.title;
 
       const p = document.createElement('p');
-      movie.description = movie.description.substring(0, 300);
-      p.textContent = `${movie.description}...`;
+      comic.description = comic.description.substring(0, 300);
+      p.textContent = `${comic.description}...`;
 
       container.appendChild(card);
       card.appendChild(h1);
