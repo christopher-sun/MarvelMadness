418925928fe8469eddf98ad0bcde0e9362f2c411
Chris Sun <csun84@gatech.edu>
2019-01-25 12:18:13 -0500

    madlibs are in there

--- script.js
+++ script.js
@@ -55,5 +55,11 @@ md5Request.onload = function () {
 }
 md5Request.send();
 
-
-
+// Madlibs
+// const Http = new XMLHttpRequest();
+// const url='https://jsonplaceholder.typicode.com/posts';
+// Http.open("GET", url);
+// Http.send();
+// Http.onreadystatechange=(e)=>{
+// console.log(Http.responseText)
+// }
